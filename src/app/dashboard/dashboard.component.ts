23fd299291f108fc21ad10bf20b3ebbf0cf76fb1
Giovanni Izzi <giovanni9972@libero.it>
2021-07-12 11:16:19 +0200

    add display

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,11 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
   constructor() { }
+  money = "0.00";
+  moneyPerson = "0.00";
 
   ngOnInit(): void {
   }
